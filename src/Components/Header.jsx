01dc4a69d3d265865f22c87ccbb60c56af77c975
Kaic Machado <kaicmachado2@gmail.com>
2024-08-29 22:21:39 -0300

    finalizado header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,19 @@
 const Header = () => {
  return (
-  <div className="bg-[url('./assets/bg.jpg')]">
-   <div className="h-72 text-white mx-auto grid grid-cols-2 font-serif items-center">
-    <h1 className="text-6xl font-bold mx-auto">
+  <div className="bg-[url('./assets/bg.jpg')] bg-center  bg-no-repeat ">
+   <div className="h-52 md:h-72 w-max-full text-white mx-auto grid grid-cols-1 sm:grid-cols-[60%_40%] font-serif items-center">
+    <h1 className="text-4xl mx-auto text-left  lg:text-6xl  font-bold ">
      Seja bem vindo
      <br />
      ao nosso cinema
     </h1>
-    <p className="text-6xl font-bold mx-auto">KFilms</p>
+    <p className=" sm:block hidden text-5xl lg:text-6xl font-bold mx-auto">
+     KFilms
+    </p>
+    <p className="text-white  font-bold text-xl lg:text-3xl col-span-full text-center ">
+     Aqui você encontrará os melhores filmes.
+    </p>
    </div>
-   <p className="font-white">
-    Aqui você encontrará os melhores filmes, séries e lançamentos.
-   </p>
   </div>
  );
 };
