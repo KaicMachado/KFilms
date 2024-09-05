ddd7e979dec31efad88c78b7e7a1d5d3f5b42a54
Kaic Machado <kaicmachado2@gmail.com>
2024-09-04 21:42:48 -0300

    consertado bug de img dos filmes ficando gigantes no mobile

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line react/prop-types
 const Card = ({ title, src }) => {
  return (
-  <div className="bg-white min-w-56 pb-3 rounded-md md:w-48 flex flex-col md:gap-4 justify-center">
-   <div>
+  <div className="bg-white md:max-w-80 pb-3 rounded-md md:w-64 flex flex-col md:gap-4 justify-center">
+   <div className="">
     <img
-     className="h-full"
+     className="max-h-96  w-full"
      src={`https://image.tmdb.org/t/p/original/${src}`}
-     alt=""
+     alt="Imagem do cartaz"
     />
    </div>
    <div className="px-3 md:pt-1">
